8344f273bba75cdcec8a42d45f4511c7994bbee8
Gen <buzhybrid@gmail.com>
2024-04-13 00:58:00 -0700

    add credits to font.nix

--- home/fonts.nix
+++ home/fonts.nix
@@ -1,9 +1,9 @@
+# credits to @uncenter for letting me nab this
 {pkgs, ...}: {
   home.packages = with pkgs; [
     (nerdfonts.override {
       fonts = [
         "CommitMono"
-        "0xProto"
         "CascadiaCode"
       ];
     })
