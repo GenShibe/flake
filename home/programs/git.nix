a33f02a447524c11d717b36b2f28842e3b5e0ca1
Gen <buzhybrid@gmail.com>
2024-04-12 20:28:04 -0700

    silly me

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -12,7 +12,7 @@ _: {
     userEmail = "29279972+GenShibe@users.noreply.github.com";
 
     extraConfig = {
-      user.signingkey = "5D63356DC78BB4CDD862F6DDCE5EAD50DD5E2278";
+      user.signingkey = "CE5EAD50DD5E2278";
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
     };
