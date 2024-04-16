59079aeb2ae283b210b6f31479d5b82bd86a0220
Gen <buzhybrid@gmail.com>
2024-04-16 10:48:44 -0700

    fix: git-lfs not working

--- modules/homebrew.nix
+++ modules/homebrew.nix
@@ -22,6 +22,7 @@
 
     brews = [
         "pinentry-mac"
+        "git-lfs"
     ];
 
     casks = [
--- modules/packages.nix
+++ modules/packages.nix
@@ -11,7 +11,6 @@
     # Essentials #
     coreutils
     curl
-    git-lfs
     gnupg
     wget
 
